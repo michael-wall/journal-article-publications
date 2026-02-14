1a8cbeb09f0fbe297d0824a2f53bc69b7ee26d0c
Michael Wall <Michael Wall@DESKTOP-FPKKMS7>
2026-02-14 10:38:06 +0000

    Add support for GROUP_IDs check. Make processing asynchronous.

--- modules/ctcollection-model-listener/src/main/java/com/mw/ctcollection/modelListener/CTCollectionModelListener.java
+++ modules/ctcollection-model-listener/src/main/java/com/mw/ctcollection/modelListener/CTCollectionModelListener.java
@@ -17,18 +17,25 @@ import com.liferay.portal.kernel.model.BaseModelListener;
 import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.transaction.TransactionCommitCallbackUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
 
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -40,7 +47,8 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
 	
-	private static final long GROUP_ID = 20117; // Externalize
+	// Site groupId(s) and / or Asset Library groupId(s)
+	private static final long[] GROUP_IDS = {20117, 34537}; // Externalize
 	
 	private static interface FIELDS {
 		static final String POST_LOGIN = "PostLogin";
@@ -57,10 +65,47 @@ public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
 		
 		return journalArticleClassName.getClassNameId();
 	}
-	
+
     @Override
     public void onAfterUpdate(CTCollection originalModel, CTCollection model) throws ModelListenerException {
     	_log.info("onAfterUpdate");
+    	
+    	boolean run = false;
+    	
+    	if (originalModel.getStatus() == WorkflowConstants.STATUS_PENDING && model.getStatus() == WorkflowConstants.STATUS_APPROVED) { // Just Published...
+    		run = true;
+    	}
+    	
+    	if (!run) return;
+    	
+    	PermissionChecker permissionChecker = PermissionThreadLocal.getPermissionChecker();
+    	
+		// Asynchronous so it doesn't prevent the Publishing from completing...
+		TransactionCommitCallbackUtil.registerCallback(new Callable<Void>() {
+		    @Override
+		    public Void call() throws Exception {
+		        Executors.newSingleThreadExecutor().submit(() -> {
+		            try {
+		            	_log.info("Starting...");
+		            	
+		            	onAfterUpdateJournalArticles(permissionChecker, originalModel, model);
+		            	
+		            	_log.info("Completed...");
+		            } catch (Exception e) {
+		                _log.error("Error", e);
+		            }
+		        });
+
+		        return null;
+		    }
+		});
+    }
+
+    private void onAfterUpdateJournalArticles(PermissionChecker permissionChecker, CTCollection originalModel, CTCollection model) {
+    	_log.info("onAfterUpdateJournalArticles");
+    	
+    	PrincipalThreadLocal.setName(permissionChecker.getUserId());
+    	PermissionThreadLocal.setPermissionChecker(permissionChecker);
 
     	// Assumes Sites have the same Default Locale as the Virtual Instance... 
     	Locale defaultLocale = getDefaultLocale(model.getCompanyId());
@@ -70,85 +115,83 @@ public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
     	Map<Long, Long> articleDeletedMap = new HashMap<Long, Long>();
     	Map<Long, Long> articleExpiredMap = new HashMap<Long, Long>();
 
-    	if (originalModel.getStatus() == WorkflowConstants.STATUS_PENDING && model.getStatus() == WorkflowConstants.STATUS_APPROVED) { // Just Published...
-    		List<CTEntry> ctEntriesList = new ArrayList<CTEntry>(_ctEntryLocalService.getCTEntries(model.getCtCollectionId(), getJournalArticleClassNameId()));
+    	List<CTEntry> ctEntriesList = new ArrayList<CTEntry>(_ctEntryLocalService.getCTEntries(model.getCtCollectionId(), getJournalArticleClassNameId()));
     		
-    		_log.info("Original Journal Articles ctEntries count: " + ctEntriesList.size());
-    		
-    		ctEntriesList.sort(Comparator.comparing(CTEntry::getCreateDate).reversed()); // Latest first...
-    		
-    		for (CTEntry ctEntry: ctEntriesList) {
-    			long modelClassPK = ctEntry.getModelClassPK();
-    			
-    			int changeType = ctEntry.getChangeType();
-    			
-    			JournalArticle journalArticle = _journalArticleLocalService.fetchArticle(modelClassPK);
-    			
-    			if (journalArticle == null) continue;
-    			
-    			_log.info(CTConstants.getCTChangeTypeLabel(changeType) + " " + journalArticle.getResourcePrimKey() + ", isInTrash: " + journalArticle.isInTrash() + ", isExpired: " + journalArticle.isExpired());
-    			
-    			// Only interested in Articles in specific site(s), ignore the rest...
-    			if (journalArticle.getGroupId() != GROUP_ID) continue;
-    			
-    			if (changeType != CTConstants.CT_CHANGE_TYPE_ADDITION && changeType != CTConstants.CT_CHANGE_TYPE_MODIFICATION && changeType != CTConstants.CT_CHANGE_TYPE_DELETION) {
-    				continue;
-    			}
-    			
-    			if (changeType == CTConstants.CT_CHANGE_TYPE_DELETION) { // Track so we can discard the deleted ones later...
-    				articleDeletedMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
-    				
-    				_log.info("deleted without trash?");
-    				
-    				continue;
-    			}
+		_log.info("Original Journal Articles ctEntries count: " + ctEntriesList.size());
+		
+		ctEntriesList.sort(Comparator.comparing(CTEntry::getCreateDate).reversed()); // Latest first...
+		
+		for (CTEntry ctEntry: ctEntriesList) {
+			long modelClassPK = ctEntry.getModelClassPK();
+			
+			int changeType = ctEntry.getChangeType();
+			
+			JournalArticle journalArticle = _journalArticleLocalService.fetchArticle(modelClassPK);
+			
+			if (journalArticle == null) continue;
+			
+			_log.info(CTConstants.getCTChangeTypeLabel(changeType) + " " + journalArticle.getResourcePrimKey() + ", isInTrash: " + journalArticle.isInTrash() + ", isExpired: " + journalArticle.isExpired());
+			
+			// Only interested in Articles in specific site(s), ignore the rest...
+			if (!groupIdsContains(journalArticle.getGroupId())) continue;
+			
+			if (changeType != CTConstants.CT_CHANGE_TYPE_ADDITION && changeType != CTConstants.CT_CHANGE_TYPE_MODIFICATION && changeType != CTConstants.CT_CHANGE_TYPE_DELETION) {
+				continue;
+			}
+			
+			if (changeType == CTConstants.CT_CHANGE_TYPE_DELETION) { // Track so we can discard the deleted ones later...
+				articleDeletedMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
+				
+				_log.info("deleted without trash?");
+				
+				continue;
+			}
 
-    			if (journalArticle.isInTrash()) { // Track so we can discard the deleted ones later...
-    				articleDeletedMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
+			if (journalArticle.isInTrash()) { // Track so we can discard the deleted ones later...
+				articleDeletedMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
 
-    				continue;
-    			}
-    			
-    			if (journalArticle.isExpired()) { // Track so we can discard the expired ones later...
-    				articleExpiredMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
+				continue;
+			}
+			
+			if (journalArticle.isExpired()) { // Track so we can discard the expired ones later...
+				articleExpiredMap.put(journalArticle.getResourcePrimKey(), journalArticle.getResourcePrimKey());
 
-    				continue;
-    			}
-    			
-    			// Check if latest already included in either map... we only care about the postLogin details of the latest version within this Publications set of changes 
-    			if (articlesMap.containsKey(journalArticle.getResourcePrimKey()) || articleDeletedMap.containsKey(journalArticle.getResourcePrimKey()) || articleExpiredMap.containsKey(journalArticle.getResourcePrimKey())) continue; 
+				continue;
+			}
+			
+			// Check if latest already included in either map... we only care about the postLogin details of the latest version within this Publications set of changes 
+			if (articlesMap.containsKey(journalArticle.getResourcePrimKey()) || articleDeletedMap.containsKey(journalArticle.getResourcePrimKey()) || articleExpiredMap.containsKey(journalArticle.getResourcePrimKey())) continue; 
 
-    			getPostLoginReferenceExpandoFields(journalArticle, defaultLocale);
-    			
-    			String postLoginReference = getPostLoginReferenceExpandoFields(journalArticle, defaultLocale);
-    				
-    			// postLogin true and postLoginReference populated so we care about this change...
-    			if (Validator.isNotNull(postLoginReference)) articlesMap.put(journalArticle.getResourcePrimKey(), journalArticle);
-    		}
-    		
-    		// The ones in articleDeletedMap shouldn't be in articlesMap but just in case...
-    		for (Long key : articleDeletedMap.keySet()) {
-    		    articlesMap.remove(key);
-    		}
+			getPostLoginReferenceExpandoFields(journalArticle, defaultLocale);
+			
+			String postLoginReference = getPostLoginReferenceExpandoFields(journalArticle, defaultLocale);
+				
+			// postLogin true and postLoginReference populated so we care about this change...
+			if (Validator.isNotNull(postLoginReference)) articlesMap.put(journalArticle.getResourcePrimKey(), journalArticle);
+		}
+		
+		// The ones in articleDeletedMap shouldn't be in articlesMap but just in case...
+		for (Long key : articleDeletedMap.keySet()) {
+		    articlesMap.remove(key);
+		}
 
-    		// The ones in articleExpiredMap shouldn't be in articlesMap but just in case...
-    		for (Long key : articleExpiredMap.keySet()) {
-    		    articlesMap.remove(key);
-    		}
-    		
-    		_log.info("Filtered Journal Articles count: " + articlesMap.size());
-    		
-    		// The final deduplicated list without deleted articles
-    		for (Long key : articlesMap.keySet()) {
-    			JournalArticle journalArticle = _journalArticleLocalService.fetchLatestArticle(key, WorkflowConstants.STATUS_APPROVED);
-    			
-    			if (journalArticle == null) {
-    				_log.info("Journal Article not found: " + key);
-    			} else {    				
-        			_log.info("Journal Article resourcePrimKey: " + journalArticle.getResourcePrimKey() + ", version: " + journalArticle.getVersion() + ", title: " + journalArticle.getTitle(defaultLanguageId) + ", structureId: " + journalArticle.getDDMStructureId() + " with postLoginReference: " + getPostLoginReferenceExpandoFields(journalArticle, defaultLocale));
-    			}    			
-    		}
-    	}
+		// The ones in articleExpiredMap shouldn't be in articlesMap but just in case...
+		for (Long key : articleExpiredMap.keySet()) {
+		    articlesMap.remove(key);
+		}
+		
+		_log.info("Filtered Journal Articles count: " + articlesMap.size());
+		
+		// The final deduplicated list without deleted articles
+		for (Long key : articlesMap.keySet()) {
+			JournalArticle journalArticle = _journalArticleLocalService.fetchLatestArticle(key, WorkflowConstants.STATUS_APPROVED);
+			
+			if (journalArticle == null) {
+				_log.info("Journal Article not found: " + key);
+			} else {    				
+    			_log.info("Journal Article resourcePrimKey: " + journalArticle.getResourcePrimKey() + ", version: " + journalArticle.getVersion() + ", title: " + journalArticle.getTitle(defaultLanguageId) + ", structureId: " + journalArticle.getDDMStructureId() + " with postLoginReference: " + getPostLoginReferenceExpandoFields(journalArticle, defaultLocale));
+			}    			
+		}
     }
     
 	private String getPostLoginReferenceExpandoFields(JournalArticle journalArticle, Locale locale) {
@@ -211,6 +254,10 @@ public class CTCollectionModelListener extends BaseModelListener<CTCollection> {
 		if (locale == null) return Locale.UK; //Fall back
 		
 		return locale;
+	}	
+	
+	public boolean groupIdsContains(long value) {
+	    return Arrays.stream(GROUP_IDS).anyMatch(id -> id == value);
 	}	
 	
 	@Reference
